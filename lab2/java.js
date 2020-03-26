751c8564d0950d1de54e9fcfab12ac964299012e
mrmukhtar <47410646+mrmukhtar@users.noreply.github.com>
2020-03-26 20:34:48 +0600

    Add files via upload

---
+++ lab2/java.js
@@ -0,0 +1,68 @@
+// var number = 4;
+
+//String
+
+//true/false
+
+//null
+
+//undefined
+
+//let obj{
+    //name: 'apple',
+    //color: 'green',
+    //weight: 200
+//
+
+//Symbol
+
+//alert(1234)
+//console.log(number);
+
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+
+// let answer = prompt("Вам есть 18?");
+// console.log(answer);
+
+// console.log(4 + 'afa');
+
+// let isChecked = true,
+//     isClose = false;
+
+// //console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+
+// if (4*2 == 8*1) {
+//     console.log('Верно')
+// } else{
+//     console.log('Ошибка')
+// }
+
+// let answer = confirm("Вам есть 18?");
+// if(answer){
+//     console.log('Проходите')
+// } else{
+//     console.log('Уходи')
+// }
+
+// const num = 50;
+// if(num < 49){
+//     console.log('Неправильно')
+// } else if (num>100) {
+//     console.log('Много')
+// } else{
+//     console.log('Верно')
+// }
+
+// for(let i = 1; i < 8; i++){
+//     console.log(i); 
+// }
+
+// function logging(a,b) {
+//     console.log(a+b)
+// }
+
+// logging(3,5);
+// logging(6,8); 
+
