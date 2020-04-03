1a3b995a47c36c55da3c49186347685f3c38d107
mrmukhtar <47410646+mrmukhtar@users.noreply.github.com>
2020-04-04 00:59:35 +0600

    Add files via upload

---
+++ hhback/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from api.views import companies_list, company_details, vacancies_list, vacancy_detail, company_vacancies, vacancies_top_ten
+
+urlpatterns=[
+    path('companies/',companies_list),
+    path('companies/<int:company_id>/', company_details),
+    path('companies/<int:company_id>/vacancies/', company_vacancies),
+    path('vacancies/',vacancies_list),
+    path('vacancies/<int:vacancy_id/>',vacancy_detail),
+    path('vacancies/top_ten/',vacancies_top_ten)
+
+]
